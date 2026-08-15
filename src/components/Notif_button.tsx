9e9bacdd45a62693211a714a67e3775cd9b96d3c
nicole-partible <261411977+nicole-partible@users.noreply.github.com>
2026-08-15 22:30:51 +0800

    Add notification datas

--- src/components/Notif_button.tsx
+++ src/components/Notif_button.tsx
@@ -1,5 +1,33 @@
+import { IoMdNotificationsOutline } from "react-icons/io";
+
 function Notif_button() {
+    const notif=[{
+        id: 1,
+            title: "Review Reminder",
+            message: "You have a Calculus review scheduled today.",
+            time: "5 min ago",
+            unread: true,
+        },
+        {
+            id: 2,
+            title: "Great job!",
+            message: "You completed your Web Development review.",
+            time: "1 hour ago",
+            unread: true,
+        },
+        {
+            id: 3,
+            title: "Upcoming Review",
+            message: "Data Structures is scheduled for tomorrow.",
+            time: "Yesterday",
+            unread: false,
+        },
+    ]
+
+
+
     return (
+
         <div></div>
     );
 }
