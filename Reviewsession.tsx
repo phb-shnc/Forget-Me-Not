7f753a87312f806be1d1baed05310ea9a618d187
Nicole <261411977+nicole-partible@users.noreply.github.com>
2026-09-25 21:48:55 +0800

    Add functionality to New Topics button

---
+++ Reviewsession.tsx
@@ -0,0 +1,14 @@
+import Navbarhome from "../components/Navbarhome";
+
+
+function Reviewsession(){
+
+
+    return(
+        <>
+        <Navbarhome />
+        <div className="bg-indigo-950 h-screen">dw</div>
+    </>
+    )
+}
+export default Reviewsession;
